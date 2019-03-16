f396404eda02566eb0c2fdef22d32714b3646191
Andrew Waller <andrewhwaller@gmail.com>
2019-03-16 03:40:29 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,5 +16,8 @@ WIN_COMBINATIONS = [
 ]
 
 def won?(board)
-  board.each do
+  WIN_COMBINATIONS.detect do |combo|
+    board[combo[0]] == board[combo[1]] &&
+    board[combo[1]] == board[combo[2]] &&
+    position_taken?(board, combo[0])
 end
